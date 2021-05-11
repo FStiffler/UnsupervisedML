af384f9cff9cb69847bc55a408efaa270a413f3c
FStiffler <fstiffler@gmx.ch>
2021-05-11 15:48:01 +0200

    First commits

---
+++ dataAnalytics.py
@@ -0,0 +1,8 @@
+# import needed packages
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
+# load data
+housingData = pd.read_csv("kc_house_data.csv")
