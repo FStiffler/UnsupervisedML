72aa92cc9713e319095b0173e685c400c77edb1e
FStiffler <fstiffler@gmx.ch>
2021-06-22 20:04:46 +0200

    deleted files

--- dataAnalytics.py
+++
@@ -1,9 +0,0 @@
-# import needed packages
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import tensorflow as tf
-
-# load data
-housingData = pd.read_csv("kc_house_data.csv")
-pd.DataFrame.head(housingData)
